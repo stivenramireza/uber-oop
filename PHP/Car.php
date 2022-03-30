2a776bd54d9ea27f7024c6786f993808bb5d5ff1
Stiven Ramírez Arango <stivenramireza@gmail.com>
2022-03-29 21:56:17 -0500

    Constructor() in PHP

--- PHP/Car.php
+++ PHP/Car.php
@@ -4,4 +4,13 @@ class Car {
     public $license = string;
     public $driver = string;
     public $passenger = string;
+
+    public function __construct($license, $driver) {
+        $this->license = $license;
+        $this->driver = $driver;
+    }
+
+    public function printDataCar() {
+        echo "License: {$this->license} - Driver {$this->driver->name}"
+    }
 }
---
+++ PHP/index.php
@@ -0,0 +1,6 @@
+<?php
+require_once("Car.php");
+require_once("Account.php")
+
+$car = new Car("AW456", new Account("Andrés Herrera", "AMS123"));
+$car->printDataCar();
