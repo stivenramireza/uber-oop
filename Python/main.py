74c5fc25822f69b6803d764843a3ad6336f1f1eb
Stiven Ramírez Arango <stivenramireza@gmail.com>
2022-03-29 19:37:27 -0500

    Creating objects in Python

--- Python/main.py
+++ Python/main.py
@@ -1,14 +1,10 @@
 from car import Car
+from account import Account
+
 
 if __name__ == "__main__":
     print("Hello world!")
 
-    car = Car()
-    car.license = "AMS234"
-    car.driver = "Andrés Herrera"
+    car = Car("AMS234", Account("Andrés Herrera", "ANDA876"))
     print(vars(car))
-
-    car2 = Car()
-    car2.license = "QWE567"
-    car2.driver = "Matha"
-    print(vars(car2))
+    print(vars(car.driver))
